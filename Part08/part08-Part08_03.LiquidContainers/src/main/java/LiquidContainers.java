0a75c780c5782ebbcc62598b6365baec56ca9b1b
llasantinelli <ich.lauri@gmail.com>
2024-08-12 19:02:59 -0300

    Upload Part08.LiquidContainers

--- Part08/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ Part08/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -3,32 +3,32 @@ import java.util.Scanner;
 
 public class LiquidContainers {
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
 
-        Container container = new Container();
-        while (true) {
-            System.out.println(container);
-            String userInput = scan.nextLine();
-            
-            String[] parts = userInput.split(" ");
-            String command = parts[0];
-            
-            if (command.equals("quit")) {
-                break;
-            }
-             
-            int value = Integer.valueOf(parts[1]);
-            
-            if(command.equals("add")) {
-                container.add(value);
-            } else if (command.equals("move")) {
-                container.move(value);
-            } else if (command.equals("remove")) {
-                container.remove(value);
-            }
+    Container container = new Container();
+    while (true) {
+      System.out.println(container);
+      String userInput = scan.nextLine();
 
-        }
+      String[] parts = userInput.split(" ");
+      String command = parts[0];
+
+      if (command.equals("quit")) {
+        break;
+      }
+
+      int value = Integer.valueOf(parts[1]);
+
+      if (command.equals("add")) {
+        container.add(value);
+      } else if (command.equals("move")) {
+        container.move(value);
+      } else if (command.equals("remove")) {
+        container.remove(value);
+      }
+
     }
+  }
 
 }
